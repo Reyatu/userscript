c30b78277313d019a2113a8746cbc90ab783bf0e
Reyatu <Reyatu@users.noreply.github.com>
2015-01-21 00:53:28 -0800

    Update AutoEconomy.user.js
    '&' と '?' 入れてみた

--- AutoEconomy.user.js
+++ AutoEconomy.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name	AutoEconomy
 // @include	http://www.nicovideo.jp/watch/*
-// @version	1.0
+// @version	1.0.1
 // @grant	none
 // ==/UserScript==
 
 var oldLocation = window.location.toString();
 
-if (!/\?eco=1$/.test(oldLocation)) {
-    window.location.replace(oldLocation + '?eco=1');
+if (!/[&\?]eco=1$/.test(oldLocation)) {
+    if (~ oldLocation.indexOf('?')) {
+	window.location.replace(oldLocation + '&eco=1');
+    } else {
+	window.location.replace(oldLocation + '?eco=1');
+    }
 }
